65082ee55f61c9f105fc5da2a4583e763792c1ba
bernardoarantes <bernardoarantes2006@gmail.com>
2026-05-23 13:43:02 -0300

    Improved JacobsonRank benchmark tester

--- benchmarks/jacobsonrank/jacobsonrank-tester.cpp
+++ benchmarks/jacobsonrank/jacobsonrank-tester.cpp
@@ -1,22 +1,23 @@
-#include "../include/jacobsonrank.h" 
-#include "../include/bitvector.h"
+#include "bitvector.h"
 #include <iostream>
 #include <chrono>
-#include <math.h>
 #include <cstring>
-#include <time.h>
 #include <cstdlib>
+#include <cmath>
 #include <algorithm>
 #include <vector>
-#include <assert.h>
 
 #ifdef _nbits
-#define _CHUNK_SIZE "NBITS e NBITS * NBITS\n"
-#elifdef _nbits512
-#define _CHUNK_SIZE "NBITS e 512 * NBITS\n"
+#define _CHUNK_SIZE "NBITS & NBITS * NBITS"
 #else
-#define _CHUNK_SIZE "LOG(N) E LOG(N) * LOG(N)\n"
+#ifdef _nbits512
+#define _CHUNK_SIZE "NBITS & 512 * NBITS"
+#else
+#define _CHUNK_SIZE "LOG(N) & LOG(N) * LOG(N)"
 #endif
+#endif
+
+using namespace std;
 
 bool randomize_bit(unsigned long i) {
     return rand() % 2 == 0;
@@ -28,6 +29,8 @@ int main(int argc, char *argv[]) {
     bool rank1 = false;
     bool select0 = false;
     bool select1 = false;
+    string operation;
+
     for (int i = 0; i < argc; i++) {
         if (std::strcmp(argv[i], "-v") == 0 || std::strcmp(argv[i], "--verbose") == 0)
             verbose = true;
@@ -35,12 +38,16 @@ int main(int argc, char *argv[]) {
         if (std::strcmp(argv[i], "-op") == 0 || std::strcmp(argv[i], "--operation") == 0) {
             if (std::strcmp(argv[i + 1], "rank0") == 0 || std::strcmp(argv[i + 1], "r0") == 0) {
                 rank0 = true;
+                operation = "RANK 0";
             } else if (std::strcmp(argv[i + 1], "rank1") == 0 || std::strcmp(argv[i + 1], "r1") == 0) {
                 rank1 = true;
+                operation = "RANK 1";
             } else if (std::strcmp(argv[i + 1], "select0") == 0 || std::strcmp(argv[i + 1], "s0") == 0) {
                 select0 = true;
+                operation = "SELECT 0";
             } else if (std::strcmp(argv[i + 1], "select1") == 0 || std::strcmp(argv[i + 1], "s1") == 0) {
                 select1 = true;
+                operation = "SELECT 1";
             }
         }
 
@@ -48,15 +55,15 @@ int main(int argc, char *argv[]) {
     }
 
     if (verbose) {
-        string header = 
-            "===============================================================\n"
-            "OS TESTES A SEGUIR USARAM CHUNK SIZE: " _CHUNK_SIZE
-            "ALEM DISSO, ELES "
+        char header[] = 
+            "=======================================================================\n"
+            "THE FOLLOWING TESTS UTILIZE CHUNK SIZE: " _CHUNK_SIZE "\n"
+            "IN ADDITION, THEY "
             #ifndef selectstructure
-            "NAO "
+            "DO NOT "
             #endif
-            "USARAM A ESTRUTURA AUXILIAR DE SELECT\n"
-            "===============================================================\n\n"
+            "UTILIZE AUXILIARY SELECT STRUCTURE\n"
+            "=======================================================================\n\n"
             ;
 
         std::cout << header;
@@ -64,66 +71,61 @@ int main(int argc, char *argv[]) {
         std::cout << "\"Size\";\"Time\"\n";
     }
 
-    for (long long unsigned size = 1000; size < 10000000000; size *= 10) {
+    for (unsigned long long order = 3; order < 10; order += 1) {
         srand(time(0));
+        unsigned long long size = pow(10, order);
         bitVector B1(size, randomize_bit);
-        int order = log10(size);
 
         B1.JacobsonRank_build();
 
-        vector<unsigned long long> times(0);
-
         #ifdef selectstructure
         if (select0) {
-            B1.build_select0();       
+            B1.build_select0();
         } else if (select1) {
-            B1.build_select1();       
+            B1.build_select1();
         }
         #endif
-        for (unsigned long long j = 0; j < 999999; j++) {
-            std::chrono::high_resolution_clock::time_point start;
-            std::chrono::high_resolution_clock::time_point end;
+        std::chrono::high_resolution_clock::time_point start;
+        std::chrono::high_resolution_clock::time_point end;
+        vector<unsigned long long> idx(999999);
+        unsigned long long upper_bound = (select0 || select1) ? size / 2 : size;
 
-            if (rank0) {
-                unsigned long long index = rand() % size;
-                start = std::chrono::high_resolution_clock::now();
-                B1.rank0(index);
-                end = std::chrono::high_resolution_clock::now();
-            } else if (rank1) {
-                unsigned long long index = rand() % size;
-                start = std::chrono::high_resolution_clock::now();
-                B1.rank1(index);
-                end = std::chrono::high_resolution_clock::now();
-            } else if (select0) {
-                unsigned long long index = rand() % (size / 2);
-                start = std::chrono::high_resolution_clock::now();
-                B1.select0(index);
-                end = std::chrono::high_resolution_clock::now();
-            } else if (select1) {
-                unsigned long long index = rand() % (size / 2);
-                start = std::chrono::high_resolution_clock::now();
-                B1.select1(index);
-                end = std::chrono::high_resolution_clock::now();
-            }
-		
-            times.push_back((end - start).count());
+        for (unsigned long long j = 0; j < 999999; j++) {
+            idx[j] = rand() % upper_bound;
         }
 
+        if (rank0) {
+            start = std::chrono::high_resolution_clock::now();
+            for (unsigned long long i : idx) {
+                B1.rank0(i);
+            }
+            end = std::chrono::high_resolution_clock::now();
+        } else if (rank1) {
+            start = std::chrono::high_resolution_clock::now();
+            for (unsigned long long i : idx) {
+                B1.rank1(i);
+            }
+            end = std::chrono::high_resolution_clock::now();
+        } else if (select0) {
+            start = std::chrono::high_resolution_clock::now();
+            for (unsigned long long i : idx) {
+                B1.select0(i);
+            }
+            end = std::chrono::high_resolution_clock::now();
+        } else if (select1) {
+            start = std::chrono::high_resolution_clock::now();
+            for (unsigned long long i : idx) {
+                B1.select1(i);
+            }
+            end = std::chrono::high_resolution_clock::now();
+        }
 
-        unsigned long long sum = 0;
-        for (unsigned long long time : times) sum += time;
+        std::chrono::duration<double, nano> elapsed_time{end - start};
+        if (verbose) { 
+            cout << "Size: " << size << "\nTime per " << operation << ": " << elapsed_time.count() / (long double) 999999 << " ns\n\n";
 
-        long double mean = (long double) sum / (long double) times.size();
-
-        if (verbose) {
-            sort(times.begin(), times.end());
-            long double mediana1 = times[249999];
-            long double mediana2 = times[499999];
-            long double mediana3 = times[749999];
-            std::cout << "Ordem do vetor de bits: " << order << "\nTempo médio: " << mean << "\nInterquartil: " << (mediana3 - mediana1) << "\n\n";
-            
         } else {
-            cout << size << ";" << mean << "\n";
+            cout << size << ";" << elapsed_time.count() / (long double) 999999 << "\n";
         }
     }
 }
