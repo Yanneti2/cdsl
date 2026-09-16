64878f660e25112051290560cfee5eaf208c0405
Yanneti2 <m258602@dac.unicamp.br>
2026-09-16 11:27:11 -0300

    Removed utils directory and its headers.

--- src/utils/binary_search.cpp
+++
@@ -1,8 +0,0 @@
-unsigned long long binarySearch(bitVector* B, unsigned long long i){
-    unsigned long long left = 0;
-    unsigned long long right = i - 1;
-
-    while(left <= right){
-        
-    }
-}
