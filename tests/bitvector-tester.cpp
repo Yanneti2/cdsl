9630d672c64229935ee81c74b49972600c8b0be7
Yanneti2 <m258602@dac.unicamp.br>
2026-09-16 00:13:54 -0300

    Fixed some compilation errors

--- tests/bitvector-tester.cpp
+++ tests/bitvector-tester.cpp
@@ -32,15 +32,14 @@ int main(int argc, char *argv[])
     // std::cout << "BV2 putted into [4] of BV:" << std::endl;
     BV.print();
 
-<<<<<<< HEAD
     //----------------------------------
     // 		Constructors	       |
     //----------------------------------
 
     string s = "11010011101000";
     BitVector* string = new BitVector(s);
-    assert(string.naive_rank1(s.size()) == 7);
-    assert(string.naive_rank0(s.size()) == 7);
+    assert(string->naive_rank1(s.size()) == 7);
+    assert(string->naive_rank0(s.size()) == 7);
 
     BitVector* string2 = new BitVector(s);
 
@@ -50,13 +49,4 @@ int main(int argc, char *argv[])
 
     assert(string==string2);
     assert(string[4]==string2[4]);
-=======
-    bitVector* a = new bitVector(1,2.0);
-    for(int i = 0; i < 5; i++){
-	    a->append0();
-	    a->append1();
-    } // 0101010101
-    // Select1(5) = 9 Select0(4) = 6
-    cout << "Naive Select1(5): " << a->naive_select1(5) << endl << "Naive Select0(4): " <<a->naive_select0(4) << endl << endl;
->>>>>>> 24-wavelet-trees-implementation
 }
